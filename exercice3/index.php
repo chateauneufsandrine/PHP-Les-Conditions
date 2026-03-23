128471f97f16ddbf24ce941c3e2ae77ce66177f4
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-23 14:28:07 +0100

    add exercice3

--- exercice3/index.php
+++ exercice3/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // EXERCICE3//////////////////////////////////////////////////////////////////////////////
 // Créer deux variables age et gender. La variable gender peut prendre comme valeur :
 
@@ -11,32 +11,48 @@
 // Vous êtes une femme et vous êtes majeur
 // Vous êtes une femme et vous êtes mineur
 // Gérer tous les cas.
-$age = 30;
-$gender = "Femme";
+$age = 12;
+$gender = "Homme";
 
-if ($age>=18 || $gender==="Femme") {
-    echo("<h1>Vous êtes une femme et vous êtes majeur</h1>");
+if ($age >= 18 && $gender === "Femme") {
+    $printable = "Vous êtes une femme et vous êtes majeur";
 }
-if($age>=18 || $gender!="Femme") {
-    echo("Vous êtes un homme et vous êtes majeur");
+if ($age >= 18 && $gender === "Homme") {
+    $printable = "Vous êtes un homme et vous êtes majeur";
 }
-if($age<=18 || $gender==="Femme") {
-    echo("Vous êtes une femme et vous êtes mineur");
+if ($age <= 18 && $gender === "Femme") {
+    $printable = "Vous êtes une femme et vous êtes mineur";
 }
-if($age>=18 || $gender!="Femme") {
-    echo("Vous êtes un homme et vous êtes majeur");
+if ($age <= 18 && $gender === "Homme") {
+    $printable = "Vous êtes un homme et vous êtes mineur";
 }
+
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h1><?= "hello" ?></h1>
+  <?php if ($age >= 18 && $gender === "Femme") {?>
+       <p>Vous êtes une femme et vous êtes majeur</p>
+  <?php } ?>
+<?php if ($age >= 18 && $gender === "Homme") { ?>
+<p>Vous êtes un homme et vous êtes majeur</p>
+<?php } ?>
+<?php if ($age <= 18 && $gender === "Femme") { ?>
+<p>Vous êtes une femme et vous êtes mineur</p>
+<?php } ?>
+<?php if ($age <= 18 && $gender === "Homme") { ?>
+<p>Vous êtes un homme et vous êtes mineur</p>
+<?php } ?>
+  
 </body>
+
 </html>
