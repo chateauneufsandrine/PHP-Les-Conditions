a7965b7e2e05f9c7846f89ce0251e644e4fe0f0a
Châteauneufsandrine <chateauneufsandrine188@gmail.com>
2026-03-24 09:52:51 +0100

    add exercice8

--- exercice8/index.php
+++ exercice8/index.php
@@ -3,6 +3,15 @@
 // Traduire ce code avec des if et des else :
 //    <?php
 //   echo ($isOk) ? 'c'est ok !!' : 'c'est pas bon !!!';
+
+$isok = false;
+
+// if ($isok) {
+//   $printable = "c'est ok !!";
+// } else {
+//     $printable = "c'est pas bon !!!";
+// }
+
 // ?> 
 
 
@@ -17,5 +26,11 @@
 </head>
 <body>
     
+<?php if ($isok) { ?>
+<h2>c'est ok !!</h2>
+<?php } else { ?>
+<h2>c'est pas bon !!!</h2>
+<?php } ?>
+
 </body>
 </html>
